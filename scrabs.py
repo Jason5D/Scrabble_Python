688fbe3b37f7d8fe8dcaedf6c6ba8f7585348766
Jason <jason5donoghue@gmail.com>
2023-09-20 08:22:55 +0100

    updates point total when word is played

--- scrabs.py
+++ scrabs.py
@@ -79,15 +79,17 @@ player_to_words = {
 }
 
 player_to_points = {}
-for player, words in player_to_words.items():
-        player_points = 0
-        for word in words:
-             player_points += score_word(word)
-        player_to_points[player] = player_points
-# print(player_to_points)
+def update_point_totals():
+    for player, words in player_to_words.items():
+            player_points = 0
+            for word in words:
+                player_points += score_word(word)
+            player_to_points[player] = player_points
+    print(player_to_points)
 
 def play_word(player, word):
      player_to_words[player].append(word)
+     update_point_totals()
 play_word("player 1", "TIME")
 
-print(player_to_words)
+# print(player_to_words)
